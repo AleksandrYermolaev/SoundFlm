0a72e114724de6030a1256c999dcb2ee4a9b41d2
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-11-10 22:08:18 +0300

    Revert "init: add webpack.config and install dependencies"
    This reverts commit 7359c62b2f27355dd05a711622e3bf03240b528a.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.html';
-import './index.scss';
-import birdsData from './modules/birds-data';
-// import logoFooter from './img/banana-bamboo_icon.png';
-// import { mult, sum } from './modules/calc';
-// const img = new Image();
-// img.src = logoFooter;
